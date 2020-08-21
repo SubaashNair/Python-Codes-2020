16845ca69524191419de49f9b16608d27a63e34b
Subaash Nair <valiban12@gmail.com>
2020-08-21 15:08:32 +0800

    Simple Exercise

---
+++ lesseroftwo.py
@@ -0,0 +1,35 @@
+#lesser of two evens(2,4) ---> 2
+#lesser of two evens(2,5) ---> 5
+
+
+#solution 1
+
+def lesser_of_two_evens(a,b):
+    new_str = [a,b]
+    if a % 2== 0 and b % 2 ==0:
+        #both numbers are even
+        return min(new_str)
+        #return the min value which is added in new_str
+    else:
+        return max(new_str)
+        #return the max value which is added in new_str
+
+print(lesser_of_two_evens(2,4))
+
+
+#Solution 2
+
+def lesser_of_two_evens(a,b):
+    if a % 2 == 0  and b % 2 == 0:
+        if a < b:
+            result = a
+        else:
+            result = b
+    else:
+        if a > b:
+            result = a
+        else:
+            result = b
+    return result
+
+print(lesser_of_two_evens(3,5))
